2937fe16ea864c38bdff51ff1f198a2043c06f8f
Don Settan <98969054+Donsettan@users.noreply.github.com>
2025-05-11 22:19:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -684,13 +684,13 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
     pre = 'filep' if settings['file_secure'] else 'file'
     if settings["button"]:
         btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}", callback_data=f'{pre}#{file["file_id"]}'
-                ),
-            ]
-            for file in files
-        ]
+    InlineKeyboardButton(
+        text=f"[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}",
+        callback_data=f'{pre}#{file["file_id"]}'
+    )
+    for file in files if 'file_id' in file
+]
+
         btn.insert(0, 
             [
                 InlineKeyboardButton(f'ǫᴜᴀʟɪᴛʏ', callback_data=f"qualities#{key}"),
